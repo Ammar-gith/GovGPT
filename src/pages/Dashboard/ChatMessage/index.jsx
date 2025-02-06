8fb4a841d64c9206bd7d3d28c100c167f702ee58
Abdul Hamid <hamidkhattak469@gmail.com>
2025-02-06 12:55:12 +0500

    Changes in ChatMessage component

--- src/pages/Dashboard/ChatMessage/index.jsx
+++ src/pages/Dashboard/ChatMessage/index.jsx
@@ -15,10 +15,34 @@
 // export default ChatMessage;
 
 
+// import { FaUser, FaRobot } from "react-icons/fa"; // Import icons
+
+// const ChatMessage = ({ sender, text }) => {
+//     const isUser = sender === "user";
+
+//     return (
+//         <div className={`flex items-start ${isUser ? "justify-end" : "justify-start"} mb-2`}>
+//             {!isUser && <FaRobot className="text-gray-500 mr-2" size={20} />} {/* Bot icon */}
+//             <div
+//                 className={`max-w-xs p-3 rounded-lg text-sm ${isUser ? "bg-darkgreen text-white" : "bg-gray-200 text-gray-900"
+//                     }`}
+//             >
+//                 {text}
+//             </div>
+//             {isUser && <FaUser className="text-darkgreen ml-2" size={20} />} {/* User icon */}
+//         </div>
+//     );
+// };
+
+// export default ChatMessage;
+
 import { FaUser, FaRobot } from "react-icons/fa"; // Import icons
 
 const ChatMessage = ({ sender, text }) => {
     const isUser = sender === "user";
+
+    // Modify response formatting
+    const formattedText = text.replace("References:", "<br /><strong>References:</strong>");
 
     return (
         <div className={`flex items-start ${isUser ? "justify-end" : "justify-start"} mb-2`}>
@@ -26,12 +50,12 @@ const ChatMessage = ({ sender, text }) => {
             <div
                 className={`max-w-xs p-3 rounded-lg text-sm ${isUser ? "bg-darkgreen text-white" : "bg-gray-200 text-gray-900"
                     }`}
-            >
-                {text}
-            </div>
+                dangerouslySetInnerHTML={{ __html: formattedText }} // Inject formatted text
+            />
             {isUser && <FaUser className="text-darkgreen ml-2" size={20} />} {/* User icon */}
         </div>
     );
 };
 
 export default ChatMessage;
+
