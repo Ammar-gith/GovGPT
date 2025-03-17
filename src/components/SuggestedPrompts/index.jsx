a0b6077a6e58ac8d19f746b00a9fade99c92faa8
Abdul Hamid <hamidkhattak469@gmail.com>
2025-03-17 15:09:15 +0500

    Reslove issed in Suggestedpromts

--- src/components/SuggestedPrompts/index.jsx
+++ src/components/SuggestedPrompts/index.jsx
@@ -1,5 +1,5 @@
 const SuggestedPrompts = ({ onPromptClick }) => {
-    const suggestedPrompts = [
+    const allSuggestedPrompts = [
         "What is the role of the Khyber Pakhtunkhwa Public Service Commission in the recruitment policy?",
         "What role does the Administrative Department play in processing deputation or inter-provincial transfer requests?",
         "What is emphasized regarding adherence to the Provincial Government's policy on deputation?",
@@ -9,7 +9,7 @@ const SuggestedPrompts = ({ onPromptClick }) => {
         <>
             <h1 className="text-3xl font-bold font-poppins text-center mb-4">What can I help with? </h1>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
-                {suggestedPrompts.map((prompt, index) => (
+                {allSuggestedPrompts.map((prompt, index) => (
                     <button
                         key={index}
                         className="p-4 bg-gray-200 rounded-lg hover:bg-gray-300 text-left"
