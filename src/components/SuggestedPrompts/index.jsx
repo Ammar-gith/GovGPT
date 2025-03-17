27b1cca9c9cf58fdd6f9c198012fcc9d9989f967
Abdul Hamid <hamidkhattak469@gmail.com>
2025-03-17 15:02:40 +0500

    suggestedPrompts insert into component

--- src/components/SuggestedPrompts/index.jsx
+++ src/components/SuggestedPrompts/index.jsx
@@ -1,11 +1,10 @@
-const suggestedPrompts = [
-    "What is the role of the Khyber Pakhtunkhwa Public Service Commission in the recruitment policy?",
-    "What role does the Administrative Department play in processing deputation or inter-provincial transfer requests?",
-    "What is emphasized regarding adherence to the Provincial Government's policy on deputation?",
-    "Where can the audit be conducted with the Collector's permission?",
-];
-
 const SuggestedPrompts = ({ onPromptClick }) => {
+    const suggestedPrompts = [
+        "What is the role of the Khyber Pakhtunkhwa Public Service Commission in the recruitment policy?",
+        "What role does the Administrative Department play in processing deputation or inter-provincial transfer requests?",
+        "What is emphasized regarding adherence to the Provincial Government's policy on deputation?",
+        "Where can the audit be conducted with the Collector's permission?",
+    ];
     return (
         <>
             <h1 className="text-3xl font-bold font-poppins text-center mb-4">What can I help with? </h1>
