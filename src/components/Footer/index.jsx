10a2f12a706794cd143f3b217cd650244dbd433b
Abdul Hamid <hamidkhattak469@gmail.com>
2025-03-21 15:32:34 +0500

    footer text and links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
     return (
         <footer className="text-center p-4 text-sm border-t">
-            <a href="https://www.kpitb.gov.pk/" target="_blank" className="text-darkgreen">© Copyright 2025 Khyber Pakhtunkhwa Information Technology Board. All Rights Reserved.</a>
+            <span className="text-darkgreen">Developed under the KP Government Innovation Fellowship Program, supported by the <Link to={"https://www.kpitb.gov.pk/"} target="_blank" className="font-medium">KP Information Technology Board</Link> and <Link to={"https://www.codeforpakistan.org/"} target="_blank" className="font-medium">Code for Pakistan</Link>.</span>
         </footer>
     );
 }
